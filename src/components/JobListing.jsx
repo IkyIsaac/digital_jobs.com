8254f1d3fab22229ba6405404d32279afc8d6dd9
IkyIsaac <isaackitomari33@gmail.com>
2024-09-22 23:31:31 +0300

    Add the Add-Job Feature

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const JobListing = ({job}) => {
   const[showFullDesc,setShowFullDesc]=useState(false)
-  let description=job.description
+  let description=job.description||''
 
   if(!showFullDesc){
     description=description.substring(0,90)+'..'
@@ -33,10 +33,10 @@ const JobListing = ({job}) => {
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
                   <FaMapMarker className="inline text-lg mb-1 mr-1"/>
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-{job.location}                </div>
+                  <i className="fa-solid fa-location-dot text-lg"></i>{job.location}
+                  </div>
                 <Link
-                  to="job"
+                  to={`/jobs/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
